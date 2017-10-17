bcba3b9e63392f0b0f15962b5688fff2baba4158
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-10-17 14:20:39 -0400

    added fixture with Bid type

--- test/message-decoder.test.js
+++ test/message-decoder.test.js
@@ -3,27 +3,31 @@ const Buffer = require('safe-buffer').Buffer
 const assert = require('assert')
 const { decodeMessage } = require('../src/message-decoder')
 const fixtures = require('./fixtures/message-hex-decoded')
-const fixture = fixtures[0]
+// const fixture = fixtures[0]
 
 describe('message-decoder', function () {
-  const messageHex = fixture.hex
-  const buffer = Buffer.from(messageHex, 'hex')
 
-  describe('decodeMessage', function () {
-    let decoded
-    try {
-      decoded = decodeMessage(buffer)
-    } catch (e) {
-      console.log(e)
-    }
-    const decodedMessage = decoded[0]
-    Object.keys(decodedMessage).forEach(key => {
-      it(`should decode "${key}" from message`, function () {
-        const value = ['sender_public_key', 'signature'].indexOf(key) !== -1 ? decodedMessage[key].toString('hex') : decodedMessage[key]
-        // console.log(`decodedMessage[${key}] = ${decodedMessage[key]}`)
-        assert.equal(value, fixture.decoded[key])
+  for(let i = 0; i < fixtures.length; i++) {
+    const fixture = fixtures[i]
+    const messageHex = fixture.hex
+    const buffer = Buffer.from(messageHex, 'hex')
+
+    describe(`decodeMessage ${i}, type=${fixture.decoded.type}`, function () {
+      let decoded
+      try {
+        decoded = decodeMessage(buffer)
+      } catch (e) {
+        console.log(e)
+      }
+      const decodedMessage = decoded[0]
+      Object.keys(decodedMessage).forEach(key => {
+        it(`should decode "${key}" from message`, function () {
+          const value = ['sender_public_key', 'signature'].indexOf(key) !== -1 ? decodedMessage[key].toString('hex') : decodedMessage[key]
+          // console.log(`decodedMessage[${key}] = ${value}`)
+          assert.equal(value, fixture.decoded[key])
+        })
       })
+      console.log(`Buffer left = ${decoded[1]}`)
     })
-    console.log(`Buffer left = ${decoded[1]}`)
-  })
+  }
 })
