e4cad7b37f22f1beb1a47e462b71916af09152da
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-10-22 09:38:39 -0400

    demo with worker-task-runner is working

--- demo/demo.js
+++ demo/demo.js
@@ -4,9 +4,11 @@ const execTask = require('worker-task-runner/src/utils/exec')
 // Mapper task will be executed per element:
 const data = [1, 2, 3, 4, 5]
 
+const workerUrl = 'worker-task-runner/src/worker'
+
 // Define the main app: create a worker and run a task:
 const app =
-  create('demo/worker')
+  create(workerUrl)
     .chain(worker => runTask(worker, 'demo/task')(data))
 
 // Execute the application (see `data.task`):
