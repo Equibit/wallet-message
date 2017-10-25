c89be51208c7e049e17f5259e9cce6b5f094af85
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-10-25 13:08:44 -0400

    updated messagePow test to use fixtures and to check message hash

--- test/message-pow.test.js
+++ test/message-pow.test.js
@@ -1,20 +1,17 @@
 const assert = require('assert')
 const messagePow = require('../src/message-pow')
+const fixtures = require('./fixtures/message-hex-decoded')
+const fixture = fixtures[3]
 const fixtureNode = require('./fixtures/hdnode')
 
 describe('messagePow', function () {
   const keyPair = fixtureNode.keyPair
 
   it('should build signed message with a nonce', function () {
-    const messageData = {
-      'type': 'Bid',
-      'timestamp': 1508263808,
-      'timestamp_nanoseconds': 442996818,
-      'sender_public_key': '',
-      'payload': '{"quantity": 5000,"price": "0.333","fill_or_kill": "no","duration": 86400}'
-    }
-    const message = messagePow(messageData, keyPair, 3)
-    console.log(`message = ${message.toString('hex')}`)
-    assert.ok(message)
+    const messageData = fixture.message
+    const message = messagePow(messageData, keyPair, 4)
+    // console.log(`message = ${message.toString('hex')}`)
+
+    assert.equal(message.toString('hex'), fixture.hex)
   })
 })
